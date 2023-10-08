9e2cdd876fd97d687be4645a014ac48124eee275
Suresh <66081909+smthakor1979@users.noreply.github.com>
2023-10-08 16:36:01 +0530

    081023_2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import { HeaderComponent } from './header/header.component';
+import {MatDrawerContainer, MatSidenavModule} from '@angular/material/sidenav';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {NgIf, NgFor} from '@angular/common';
+import { MenubarComponent } from './menubar/menubar.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import { MatDrawer } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidenavComponent,
-    HeaderComponent,
-    
+    MenubarComponent,
     
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule
 
   ],
   providers: [],
